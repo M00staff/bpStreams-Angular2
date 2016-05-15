ad1f69194863e17c499d797fea2a7294b5bd43b9
M00staff <Mostafa@nete.com>
2016-05-15 01:03:33 -0400

    api calls working, nowt troubleshooting the new ng repeat

--- app/shows/shows.component.js
+++ app/shows/shows.component.js
@@ -10,21 +10,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var http_1 = require('@angular/http');
-require('rxjs/Rx');
+// import { showsService } from './shows.service';
+// import 'rxjs/Rx';
 var showsComponent = (function () {
     // constructor(private showsService: showsService) {}
     function showsComponent(jsonp) {
         var _this = this;
         this.jsonp = jsonp;
         this.pickYear = function (year, row) {
-            _this.year = year;
-            _this.row = row;
-            _this.showList = _this.jsonp.request('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:' + year + '&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=' + row + '&page=1&output=json&callback=JSONP_CALLBACK')
+            _this.jsonp.request('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:' + year + '&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=' + row + '&page=1&output=json&callback=JSONP_CALLBACK')
                 .subscribe(function (res) {
                 _this.shows = res.json();
                 console.log(_this.shows);
             });
-            // console.log(this.showList)
         };
     }
     showsComponent = __decorate([
--- app/shows/shows.component.ts
+++ app/shows/shows.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Response, JSONP_PROVIDERS, Jsonp, Http } from '@angular/http';
-import { showsService } from './shows.service';
-import 'rxjs/Rx';
+import { Response, JSONP_PROVIDERS, Jsonp } from '@angular/http';
+
+// import { showsService } from './shows.service';
+// import 'rxjs/Rx';
 
 @Component({
   selector: 'pick-shows',
@@ -17,20 +18,13 @@ export class showsComponent {
   constructor (private jsonp: Jsonp) {}
 
     public shows;
-    public year;
-    public row;
-    public showList;
 
-      pickYear = (year, row) => {
-      this.year = year;
-      this.row = row;
-      this.showList = this.jsonp.request('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ year +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ row +'&page=1&output=json&callback=JSONP_CALLBACK')
+    pickYear = (year, row) => {
+      this.jsonp.request('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ year +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ row +'&page=1&output=json&callback=JSONP_CALLBACK')
       .subscribe((res: Response) => {
                   this.shows = res.json();
                   console.log(this.shows)
                 })
-      // console.log(this.showList)
-
    }
 
 
